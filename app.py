8f0f8ac4bfa95015ece30807efe3e80e0f24b934
Dhruv9512 <dhruvsharma56780@gmail.com>
2024-08-26 14:59:20 +0530

    dcs

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import psycopg2
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": ["https://my-portfilio.onrender.com", "http://127.0.0.1:5500"]}})
+CORS(app, resources={r"/api/*": {"origins": ["http://127.0.0.1:5500", "https://my-portfilio.onrender.com"]}})
 
 # Configuration from environment variables
 app.config['POSTGRES_HOST'] = os.getenv('DB_HOST')
@@ -12,22 +12,27 @@ app.config['POSTGRES_USER'] = os.getenv('DB_USER')
 app.config['POSTGRES_PASSWORD'] = os.getenv('DB_PASSWORD')
 app.config['POSTGRES_DB'] = os.getenv('DB_NAME')
 
-
 def connect_to_database():
-    conn = psycopg2.connect(
-        host=app.config['POSTGRES_HOST'],
-        user=app.config['POSTGRES_USER'],
-        password=app.config['POSTGRES_PASSWORD'],
-        dbname=app.config['POSTGRES_DB']
-    )
-    return conn
-
+    try:
+        conn = psycopg2.connect(
+            host=app.config['POSTGRES_HOST'],
+            user=app.config['POSTGRES_USER'],
+            password=app.config['POSTGRES_PASSWORD'],
+            dbname=app.config['POSTGRES_DB']
+        )
+        return conn
+    except Exception as e:
+        app.logger.error(f"Database connection error: {str(e)}")
+        return None
 
 @app.route('/api', methods=["POST"])
 def api():
     if request.method == "POST":
+        conn = connect_to_database()
+        if not conn:
+            return jsonify({'error': 'Database connection error'}), 500
+        
         try:
-            conn = connect_to_database()
             cur = conn.cursor()
             cur.execute('SELECT * FROM projects')  
             data = cur.fetchall()
@@ -48,10 +53,10 @@ def api():
             return jsonify(json_data)
 
         except Exception as e:
-            return jsonify({'error': str(e)})
+            app.logger.error(f"Query execution error: {str(e)}")
+            return jsonify({'error': str(e)}), 500
     else:
         return jsonify({'error': 'Method not allowed'}), 405
 
-
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
